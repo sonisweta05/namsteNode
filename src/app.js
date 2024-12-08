669ea74cde4e6417edb800961f52961bb4b5741f
shubhamsoni8769 <sonisweta94@gmail.com>
2024-12-08 11:04:59 +0530

    explore get,postand delete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Sweta", lastName: "Soni" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data Successfully saved to the Server");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data Successfully deleted");
+});
+app.use("/test", (req, res) => {
+  res.send("1234 from test234");
+});
 
-app.use("/test",(req,res)=> {
-    res.send("1234 from test")
-})
-app.use("/home",(req,res)=> {
-    res.send("from home calling 1234")
-})
-app.use("/",(req,res)=> {
-    res.send("1234")
-})
-
-app.listen(3000, ()=>{
-    console.log("sucessfully");
-    
-})
+app.listen(3000, () => {
+  console.log("sucessfully");
+});
